f220d2c0d8a0d432952bd53a0188e7b7256f1492
Ferdi Mehmedov <FerdiIt@pm.me>
2024-03-16 10:25:07 +0200

    Create test for Verify Login button is visible

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -9,3 +9,13 @@ test("Verify All Books link is visible", async ({ page }) => {
 
     expect(isVisibleAllBooksLink).toBe(true);
 }); 
+
+test("Verify Login button is visible", async ({ page }) => {
+    await page.goto('http://localhost:3001');
+    await page.waitForSelector('nav.navbar');
+
+    const loginButton = await page.$('a[href="/login"]');
+    const isVisibleLoginButton = await loginButton.isVisible();
+
+    expect(isVisibleLoginButton).toBe(true);
+}); 
